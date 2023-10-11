bf4deb5d579874c17c035e41c82f7d8d44c516e6
CAR TEAM <CAR TEAM@DESKTOP-RFVDE3D>
2023-10-11 15:17:13 +0530

    Add project files.

---
+++ automation script_1/automation script_1.cs
@@ -0,0 +1,22 @@
+namespace automation_script_1
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Text;
+	using Skyline.DataMiner.Automation;
+	
+	/// <summary>
+	/// Represents a DataMiner Automation script.
+	/// </summary>
+	public class Script
+	{
+		/// <summary>
+		/// The script entry point.
+		/// </summary>
+		/// <param name="engine">Link with SLAutomation process.</param>
+		public void Run(IEngine engine   {
+                engine.GenerateInformation("Hello world!");
+            }
+	}
+}
